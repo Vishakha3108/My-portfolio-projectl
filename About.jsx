7ffc105f395991242811d9047a1cecaeee3d32b3
Vishakhabhojak <137413048+Vishakha3108@users.noreply.github.com>
2025-01-04 19:22:58 +0530

    Add files via upload

---
+++ About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function About () {
+  return (
+    <>   
+     <div className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-14">
+      <h2 className='text-3xl font-bold mb-5'>About</h2>
+      <p>Hello, I'm Vishakha a passionate Web Devloper with a Keen Eye For MERN Stack. With a background in IT, I Strive to creat impactful and visually stunning software solution that leave a lasting impression.</p>
+      <br />
+      <h2 className='text-green-500 font-semibold text-xl'>Education & Traning</h2>
+    </div>
+    </>
+  )
+}
+
+export default About
